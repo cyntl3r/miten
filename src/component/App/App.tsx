0ca542e50c0bba38eec7fdaeb5d3387584f4621b
cyntl3r <damian@cyntler.com>
2020-10-12 11:34:37 +0200

    Update App component

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,3 +1,21 @@
 import React from 'react';
+import './App.styles.css';
+import { Route, Switch } from 'react-router-dom';
+import { BrandList } from '../BrandList/BrandList';
+import { NotFound } from '../NotFound/NotFound';
+import brands from '../../brands.json';
+import { BrandsContainer } from '../BrandsContainer/BrandsContainer';
 
-export const App = () => <div>Miten Shops</div>;
+export const App = () => (
+  <Switch>
+    <Route path="/" exact>
+      <BrandList brands={brands} />
+    </Route>
+    <Route path="/brand/:tag">
+      <BrandsContainer brands={brands} />
+    </Route>
+    <Route>
+      <NotFound />
+    </Route>
+  </Switch>
+);
